78af3976a48bbf19923cda66388084b985d15499
Shiva Vishwakarma <shivavishwakarma9291@gmail.com>
2024-09-22 16:25:29 +0530

    add laravel ui files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('role')->default(0)->comment('2 - Admin,1 - Client,0 - User');
             $table->rememberToken();
             $table->timestamps();
         });
